be9025ec1e02d63bc5c70f6005a7b5a710c89865
okhomyk <oleh.khomyk@gmail.com>
2026-05-18 14:53:59 +0300

    Implemented add Post comments functionality.

--- src/features/post-comments/ui/PostComments.tsx
+++ src/features/post-comments/ui/PostComments.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo } from "react";
 import { flatMap, isNil, take } from "lodash";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
